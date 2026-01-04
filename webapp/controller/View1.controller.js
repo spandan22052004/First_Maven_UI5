fbf82a2495bfcd059934bd060b20c46755f4c9f0
Spandan Bandyopadhyay <spandan@maventic.com>
2026-01-04 04:26:07 +0000

    ui5_day2_task_appending_in_List_and_table

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,59 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller,JSONModel) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageBox, JSONModel) => {
     "use strict";
 
     return Controller.extend("sample.project1.controller.View1", {
-        onInit : function() {
-            
+        onInit() {
+            // MessageBox.success("OnInit");
+            // let oData = {
+            //     "name": "Spandan"
+            // }
+            // let oModel = new JSONModel(oData);
+            // this.getView().setModel(oModel, "model1");
+        },
+        // onBeforeRendering(){
+        //     MessageBox.success("On before rendering");
+        // },
+        // onAfterRendering(){
+        //     MessageBox.error("On after rendering");
+        // }
+        onPress: function () {
+            console.log("Button clicked");
+        },
+        onAddToTable: function () {
+            var oFormData = this.getView().getModel("formModel").getData();
+            var oTableModel = this.getView().getModel("tableModel");
+            var aItems = oTableModel.getProperty("/items");
+
+            aItems.push({ ...oFormData });
+            oTableModel.setProperty("/items", aItems);
+
+            this._clearForm();
+        },
+
+        onAddToList : function(){
+           var oFormData = this.getView().getModel("formModel").getData();
+           var oListModel = this.getView().getModel("listModel");
+           var aItems = oListModel.getProperty("/items");
+
+           aItems.push({...oFormData});
+           oListModel.setProperty("/items",aItems);
+
+           this._clearForm();
+        },
+
+        _clearForm: function () {
+            this.getView().getModel("formModel").setData({
+                name: "",
+                age: "",
+                city: ""
+            });
         }
+
+
+
     });
 });
